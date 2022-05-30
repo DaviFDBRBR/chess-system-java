5747213ef043b6df02d67df990e7eaaff2b75586
Davi Fonseca Darienzo <davireshiram3@hotmail.com>
2022-05-30 14:31:36 -0300

    Chess layer and printing the board

---
+++ src/chess/ChessMatch.java
@@ -0,0 +1,27 @@
+package chess;
+
+import boardgame.Board;
+
+public class ChessMatch {
+	private Board board;
+
+	public ChessMatch() {
+		board = new Board(8, 8);
+	}
+
+	/*
+	 * Devido ao desenvolvimento em camadas, não é possível manipular a matriz de tipo Piece no pacote/camada chess.
+	 * Então se faz um downcast dos objetos da superclasse Piece, para ChessPiece, que são classes passíveis de
+	 * manipulação na camada chess. O programa (dentro da camada chess) apenas irá enxergar ChessPiece.
+	*/
+	public ChessPiece[][] getPieces() { 
+		ChessPiece mat[][] = new ChessPiece[board.getRows()][board.getColumns()];
+		for (int i = 0; i < board.getRows(); i++) {
+			for (int j = 0; j < board.getColumns(); j++) {
+				mat[i][j] = (ChessPiece) board.piece(i, j);
+			}
+		}
+		return mat;
+	}
+	
+}
