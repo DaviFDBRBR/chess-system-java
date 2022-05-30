197232c6e01fa9c7e666f1f1d221e807c37d1a47
Davi Fonseca Darienzo <davireshiram3@hotmail.com>
2022-05-30 15:04:44 -0300

    Placing pieces on the board

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,20 +1,26 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 	private Board board;
 
 	public ChessMatch() {
 		board = new Board(8, 8);
+		initialSetup();
 	}
 
 	/*
-	 * Devido ao desenvolvimento em camadas, não é possível manipular a matriz de tipo Piece no pacote/camada chess.
-	 * Então se faz um downcast dos objetos da superclasse Piece, para ChessPiece, que é uma classe passível de
-	 * manipulação na camada chess. O programa (dentro da camada chess) apenas irá enxergar ChessPiece.
-	*/
-	public ChessPiece[][] getPieces() { 
+	 * Devido ao desenvolvimento em camadas, não é possível manipular a matriz de
+	 * tipo Piece no pacote/camada chess. Então se faz um downcast dos objetos da
+	 * superclasse Piece, para ChessPiece, que é uma classe passível de manipulação
+	 * na camada chess. O programa (dentro da camada chess) apenas irá enxergar
+	 * ChessPiece.
+	 */
+	public ChessPiece[][] getPieces() {
 		ChessPiece mat[][] = new ChessPiece[board.getRows()][board.getColumns()];
 		for (int i = 0; i < board.getRows(); i++) {
 			for (int j = 0; j < board.getColumns(); j++) {
@@ -23,5 +29,11 @@ public class ChessMatch {
 		}
 		return mat;
 	}
-	
+
+	private void initialSetup() {
+		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+	}
+
 }
