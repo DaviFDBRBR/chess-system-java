5edb1069e08a3ba510d7b2888ae599c41d5453ca
Davi Fonseca Darienzo <davireshiram3@hotmail.com>
2022-06-07 13:45:44 -0300

    Opponent Color fix

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -141,7 +141,7 @@ public class ChessMatch {
 	}
 
 	private Color opponent(Color color) {
-		return (color == Color.WHITE) ? Color.BLACK : color.WHITE;
+		return (color == Color.WHITE) ? Color.BLACK : Color.WHITE;
 	}
 
 	private ChessPiece king(Color color) {
